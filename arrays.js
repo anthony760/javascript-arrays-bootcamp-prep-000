c92448848909155748cf0d2759cd21035af8bba4
Anthony Lopez <anthony760@gmail.com>
2017-10-06 23:48:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+fucntion destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
